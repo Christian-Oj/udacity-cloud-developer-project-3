8b7bd21f19c4d267f32658a01dfcd0fb3c0985fb
Christian Ojiezele <christianojiezele@gmail.com>
2022-09-18 16:32:15 +0100

    updated the environment file

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a01c7c9a6a38642c3b7bc98f18dd88d4-1508032809.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
